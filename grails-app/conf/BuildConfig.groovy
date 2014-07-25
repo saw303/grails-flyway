15d9831baa9439b18ab1050ffbc5b1e81b078c6f
Silvio Wangler <silvio.wangler@gmail.com>
2014-07-25 17:20:53 +0200

    set target dir

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,4 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grgrails.project.work.dir = 'target'
 
 grails.project.repos.default = "artifactory"
 grails.project.repos.artifactory.url = "http://repo.wangler.io/repo"
